8f7fa7d302bf67f5be229e2817dd3d82625616f3
toxic123MMM <aashish02@gmail.com>
2024-10-14 17:46:29 +0400

    Add files via upload

---
+++ Divisble Number.py
@@ -0,0 +1,8 @@
+print("Enter a number for Numerator")
+numn=int(input())
+print("Enter a number for Denominator")
+numd=int(input())
+if numn%numd== 0:
+    print("numn is completely divisble by numd")
+else:
+     print("numn is not divisble by numd")
